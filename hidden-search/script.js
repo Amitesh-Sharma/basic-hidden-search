b9fa1dd1a191b922d9a8d90ca4419822122ac74f
Amitesh sharma <66667018+Amitesh-Sharma@users.noreply.github.com>
2021-01-23 20:03:24 +0530

    Add files via upload

---
+++ hidden-search/script.js
@@ -0,0 +1,9 @@
+const search = document.querySelectorAll('.search')
+const btn = document.querySelector('.btn')
+const input = document.querySelector('.input')
+
+btn.addEventListener('click', () => {
+    search.addEventListener.toggle('active')
+    input.focus()
+
+})
